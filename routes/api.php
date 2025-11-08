67cac8adbc970cd85adb3152e7fc7fab426d7155
HAMZA854-maker <hamza25ali26@gmail.com>
2025-11-08 09:48:26 +0500

    updates

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use App\Http\Controllers\Admin\SkuController;
 use App\Http\Controllers\Admin\BatchController;
 
 
-Route::get('/getAllBatch', [SkuController::class,'getAllBatch']);
+Route::get('/getAllSku', [SkuController::class,'getAllSku']);
 Route::get('/getAllBatch', [SkuController::class,'getAllBatch']);
